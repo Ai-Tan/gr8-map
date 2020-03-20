95674ae0cbcdfffec147101552f8f3cffe1e2837
tantan <tantant5656@gmail.com>
2020-03-20 21:28:53 +0900

    pre新規登録画面

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,25 @@
 class MapsController < ApplicationController
+  def new
+    @map = Map.new # フォーム用の空のインスタンスを生成する。
+  end
+
+  def create
+    @map = Map.new(map_params) # ストロングパラメータを引数に
+    @map.save # saveをしてデータベースに保存する。
+    redirect_to @map # showページにリダイレクト
+  end
+
+  def show
+    # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
+    # 取得したデータを@itemというインスタンス変数にセットする
+    @map = Map.find(params[:id])
+  end
+
+
+  private
+
+  def map_params # ストロングパラメータを定義する
+    params.require(:map).permit(:name, :type, :price, :closing_day, :open_time, :close_time, :website, :photo1, :photo2, :photo3, :beji, :tel, :parking, :address)
+  end
+
 end
